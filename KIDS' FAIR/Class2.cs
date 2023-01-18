f0811e954d655986a3a6bad7f499d57d28c797cb
Ryustem Shaban <rstmduran@gmail.com>
2023-01-18 20:32:10 +0100

    Started with Pet class

---
+++ KIDS' FAIR/Class2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Pet
+{
+    /*Declaring all variables*/
+    private string name = " ";
+    private int age = 0;
+    private bool isFemale = true;
+
+    /*Method for reading name of the users pet*/
+    public string ReadName()
+    {
+        Console.WriteLine("Greetings from the Movie Object! :)" + "\n-----------");
+        Console.WriteLine("What is the name of your pet? ");
+        name = Console.ReadLine();
+        /*Console.WriteLine("Fetching name -OK" + "\n-----------");*/
+        return name;
+
+    }
+}
