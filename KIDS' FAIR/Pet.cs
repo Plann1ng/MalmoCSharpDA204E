f93ebbbb6097d7c7ed336d993a09275cea19fb58
Ryustem Shaban <rstmduran@gmail.com>
2023-01-18 20:57:59 +0100

    Finalized pet object

--- KIDS' FAIR/Pet.cs
+++ KIDS' FAIR/Pet.cs
@@ -3,18 +3,53 @@
 public class Pet
 {
     /*Declaring all variables*/
-    private string name = " ";
-    private int age = 0;
+    private string name;
+    private int age;
     private bool isFemale = true;
 
     /*Method for reading name of the users pet*/
-    public string ReadName()
+    public void getNameInput()
     {
-        Console.WriteLine("Greetings from the Movie Object! :)" + "\n-----------");
+        Console.WriteLine("Greetings from MyPet class! :)\n");
         Console.WriteLine("What is the name of your pet? ");
         name = Console.ReadLine();
         /*Console.WriteLine("Fetching name -OK" + "\n-----------");*/
-        return name;
-
     }
+
+    public void getAgeInput()
+    {
+        Console.WriteLine($"What is {name}'s age? ")
+        age = int.Parse(Console.ReadLine());
+    }
+
+    public void getGenderinput()
+    {
+        Console.WriteLine("Is your pet a female? (y/n)?")
+        string userAnswer = Console.ReadLine();
+        if ( userAnswer == "y" || userAnswer == "Y")
+        {
+            isFemale= true;
+        }
+        else if (userAnswer == "n" || userAnswer == "N")
+        {
+            isFemale= false;
+        }
+        else
+        {
+            Console.WriteLine("Please answer with only <y> or <n>");
+            getGenderinput
+        }
+    }
+
+    public void Displayinformation()
+    {
+        Console.WriteLine("\n+++++++++++++++++++++++++++++");
+        Console.WriteLine($"Name: {name} Age: {age}");
+        if (isFemale)
+            Console.WriteLine($"{name} is a good girl");
+        else
+            Console.WriteLine($"{name} is a goot boy");
+        Console.WriteLine("\n+++++++++++++++++++++++++++++");
+    }
+}
 }
